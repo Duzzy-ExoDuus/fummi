ef7d5b36fedaff2cdb03b035a7a5d4a8810bfd5a
toonsim <toon.simoens@gmail.com>
2019-12-18 16:31:15 +0100

    ratings bij liedjes

--- client/src/components/general/Track.js
+++ client/src/components/general/Track.js
@@ -5,6 +5,47 @@ import {Collapse, Progress} from 'reactstrap'
 import styled from "styled-components";
 
 
+import {makeStyles} from '@material-ui/core/styles';
+import Rating from '@material-ui/lab/Rating';
+import Tooltip from '@material-ui/core/Tooltip';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+
+
+const labels = {
+    0.5: 'Useless',
+    1: 'Useless+',
+    1.5: 'Poor',
+    2: 'Poor+',
+    2.5: 'Ok',
+    3: 'Ok+',
+    3.5: 'Good',
+    4: 'Good+',
+    4.5: 'Excellent',
+    5: 'Excellent+',
+};
+
+function IconContainer(props) {
+    const {value, ...other} = props;
+    return (
+        <Tooltip title={labels[value] || ''}>
+            <span {...other} />
+        </Tooltip>
+    );
+}
+
+IconContainer.propTypes = {
+    value: PropTypes.number.isRequired,
+};
+
+const useStyles = makeStyles({
+    rating1: {
+        width: 200,
+        display: 'flex',
+        alignItems: 'center',
+    },
+});
+
 const ListItem = styled.div`
     width:80vw;
     height:12vw;
@@ -86,14 +127,18 @@ export const StyledProgressBar = styled(Progress)`
   }
 `;
 
+
 class Track extends Component {
 
     state = {
         playingPreview: false,
-        featuresDisplayed: false
+        featuresDisplayed: false,
+        rating: null
     };
 
     render() {
+
+
         const {artists, name, preview_url, album} = this.props.track;
         let artistList = artists.map(artist => artist.name + ', ');
         artistList[artistList.length - 1] = artistList[artistList.length - 1].substring(0, artistList[artistList.length - 1].length - 2);
@@ -150,7 +195,19 @@ class Track extends Component {
                 {
                     this.props.audioFeatures &&
                     <Collapse isOpen={this.state.featuresDisplayed}>
+                        <br/>
+                        <Box component="fieldset" mb={3} borderColor="transparent">
+                            <Typography component="legend">Rate This Song</Typography>
+                            <Rating name={name} value={this.state.rating == null ? "5" : this.state.rating} max={10}
+                                    onChange={
+                                        e => {
+                                            this.setState({rating: e.target.value});
+                                            console.log(e.target.value)
+                                        }
+                                    }/>
+                        </Box>
                         <div>
+
                             Acousticness:
                             <Progress color='success'
                                       value={this.props.audioFeatures.acousticness * 100}/>
@@ -161,10 +218,10 @@ class Track extends Component {
                             <Progress striped color="info" value={this.props.desiredFeatures[1]}/>
                             Energy: <Progress color='success' value={this.props.audioFeatures.energy * 100}/>
                             <Progress striped color="info" value={this.props.desiredFeatures[2]}/>
-                            instrumentalness: <Progress color='success'
+                            Instrumentalness: <Progress color='success'
                                                         value={this.props.audioFeatures.instrumentalness * 100}/>
                             <Progress striped color="info" value={this.props.desiredFeatures[3]}/>
-                            speechiness: <Progress color='success' value={this.props.audioFeatures.speechiness * 100}/>
+                            Speechiness: <Progress color='success' value={this.props.audioFeatures.speechiness * 100}/>
                             <Progress striped color="info" value={this.props.desiredFeatures[4]}/>
                             Tempo: <Progress color='success' value={(this.props.audioFeatures.tempo - 50) / 1.7}/>
                             <Progress striped color="info" value={this.props.desiredFeatures[5]}/>
