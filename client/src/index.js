c075ac5950a8d68dec9f20f318d5ae0477c00508
toonsim <toon.simoens@gmail.com>
2019-12-26 20:58:58 +0100

    Redux tools

--- client/src/index.js
+++ client/src/index.js
@@ -10,9 +10,9 @@ import App from './App';
 const middleware = [thunk];
 
 const store = createStore(rootReducer, compose(
-    applyMiddleware(...middleware),
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-    window.__REDUX_DEVTOOLS_EXTENSION__()));
+    applyMiddleware(...middleware)));
+    //window.__REDUX_DEVTOOLS_EXTENSION__ &&
+    //window.__REDUX_DEVTOOLS_EXTENSION__()
 
 ReactDOM.render(
     <Provider store={store}>
